9423d5429dabed0ca122519173e106b2488ceeb0
asiaexperiments <asiaokatun@gmail.com>
2025-04-25 12:40:39 -0400

    Updating project

---
+++ region_mapping.py
@@ -0,0 +1,24 @@
+
+# region_mapping.py
+
+
+adm2_to_region = {
+    "SO0101": "Lower Juba", "SO0102": "Lower Juba", "SO0103": "Lower Juba",
+    "SO0201": "Lower Shabelle", "SO0202": "Lower Shabelle", "SO0203": "Lower Shabelle",
+    "SO0301": "Sanaag", "SO0302": "Sanaag", "SO0303": "Sanaag",
+    "SO0401": "Togdheer", "SO0402": "Togdheer", "SO0403": "Togdheer",
+    "SO0501": "Woqooyi Galbeed", "SO0502": "Woqooyi Galbeed",
+    "SO0601": "Awdal", "SO0602": "Awdal",
+    "SO0701": "Hiraan", "SO0702": "Hiraan", "SO0703": "Hiraan",
+    "SO0801": "Middle Shabelle", "SO0802": "Middle Shabelle",
+    "SO0901": "Middle Juba", "SO0902": "Middle Juba",
+    "SO1001": "Bari", "SO1002": "Bari", "SO1003": "Bari",
+    "SO1101": "Bakool", "SO1102": "Bakool", "SO1103": "Bakool",
+    "SO1201": "Banadir", "SO1202": "Banadir",
+    "SO1301": "Sool", "SO1302": "Sool",
+    "SO1401": "Nugaal", "SO1402": "Nugaal",
+    "SO1501": "Mudug", "SO1502": "Mudug",
+    "SO1601": "Galgaduud", "SO1602": "Galgaduud",
+    "SO1701": "Gedo", "SO1702": "Gedo", "SO1703": "Gedo",
+    "SO1801": "Bay", "SO1802": "Bay", "SO1803": "Bay"
+}
